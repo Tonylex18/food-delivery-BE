201b45cfd2cb0dba07ef84d19ad9742433dc5e46
Tonylex18 <anthonyagada2000@gmail.com>
2025-02-10 19:23:45 +0100

    debugging

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,7 +2,9 @@ import orderModel from "../models/orderModel.js"
 import userModel from "../models/userModel.js"
 import Stripe from "stripe"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-06-20",
+  });
 
 // placing user order from frontend
 const placeOrder = async (req, res) => {
